ab9c657a6e56c1d15d14a1b6370676f21a7005d6
simonesinceri <56643807+simonesinceri@users.noreply.github.com>
2021-10-02 18:56:17 +0200

    modify ass6

--- Assignment_6/ass6.m
+++ Assignment_6/ass6.m
@@ -5,20 +5,18 @@ W2 = tf([2],[1 10]);
 W1 = tf([1],[1 1]);
 P = tf([1],[1 -1]);
 C = 5/7;
+beta = 2;
 
 L = P*C;
 T = L/(1+L);
 S = 1/(1+L);
-figure(7)
+figure(1)
 %bode(2*W2*T)
-bode(W2*T)
+bode(beta*W2*T)
 hold on
-%bode((W1*S)/(1-2*W2*T))
-%bode((W1*S)/(1+2*W2*T))
 
-%%
-%sfigure(1)
-%bode(1/(P*(W2-1))) % *2
+
+%% Bode per vari valori del controllore
 
 figure(2)
 C = -30;
@@ -27,15 +25,15 @@ for i= 0:1:20
     L = P*C;
     T = L/(1+L);
     hold on
-    bode(2*W2*T)
+    bode(beta*W2*T)
     C = C-i;
     
 end
 
 %%
+
 C = -0.7;
 figure(3)
-
 bode(2*W2*T)
 % nyquist(W2*T)
 
